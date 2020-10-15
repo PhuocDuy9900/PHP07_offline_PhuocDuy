0d7248f669f1278f1083037123caf071e15afdb8
PhuocDuy9900 <tranphuocduy9900@gmail.com>
2020-10-15 20:38:10 +0700

    01-menu-level-3-menuLinh

--- b04/01-menu-level-3-menuLinh/html/menuxxx.php
+++ b04/01-menu-level-3-menuLinh/html/menuxxx.php
@@ -2,13 +2,28 @@
 require_once('data.php');
 
 $xhtml       = '';
+$classActive = '';
+// lay gia tri keyLevelTwo de so sanh truoc khi tao the cha (about)
 
 
 // tao menu
 foreach ($arrMenu as $keyLevelOne => $menuLevelOne) {
     if (isset($menuLevelOne['child'])) {
 
-        $xhtml .= sprintf('<li><a href="%s">%s</a><ul>', $menuLevelOne['link'], $menuLevelOne['name']);
+        $classActive = ($menuCurrent == $keyLevelOne) ? 'class="active"' : '';
+
+        if (isset($menuLevelOne['child'][$menuCurrent])) $classActive = 'class="active"';
+        // foreach($active as $key => $value) {
+        // echo '<pre>';
+        // print_r($value);
+        // echo '</pre>';
+        //     if(isset($value[$menuCurrent])) $classActive = 'class="active"';
+        // }
+        foreach ($menuLevelOne['child'] as $menuLevelTwo) {
+            if (isset($menuLevelTwo['child'][$menuCurrent])) $classActive = 'class="active"';
+        }
+
+        $xhtml .= sprintf('<li %s><a href="%s">%s</a><ul>', $classActive, $menuLevelOne['link'], $menuLevelOne['name']);
         foreach ($menuLevelOne['child'] as $keyLevelTwo => $menuLevelTwo) {
             if (isset($menuLevelTwo['child'])) {
 
@@ -25,7 +40,7 @@ foreach ($arrMenu as $keyLevelOne => $menuLevelOne) {
         $xhtml .= '</ul></li>';
     } else {
         $classActive = ($menuCurrent == $keyLevelOne) ? 'class="active"' : '';
-        $xhtml .= sprintf('<li><a href="%s">%s</a></li>', $menuLevelOne['link'], $menuLevelOne['name']);
+        $xhtml .= sprintf('<li %s><a href="%s">%s</a></li>', $classActive, $menuLevelOne['link'], $menuLevelOne['name']);
     }
 }
 ?>
