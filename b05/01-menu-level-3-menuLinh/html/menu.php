517b7fd24f5f3b56c43ba2a988784ac006671d26
PhuocDuy9900 <tranphuocduy9900@gmail.com>
2020-10-15 20:45:43 +0700

    changed folder

--- b05/01-menu-level-3-menuLinh/html/menu.php
+++ b05/01-menu-level-3-menuLinh/html/menu.php
@@ -8,24 +8,24 @@ $xhtml       = '';
 foreach ($arrMenu as $keyLevelOne => $menuLevelOne) {
     if (isset($menuLevelOne['child'])) {
 
-        $xhtml .= sprintf('<li><a href="%s">%s</a><ul>', $menuLevelOne['link'], $menuLevelOne['name']);
+        $xhtml .= sprintf('<li data-name="%s"><a href="%s">%s</a><ul>', $keyLevelOne, $menuLevelOne['link'], $menuLevelOne['name']);
         foreach ($menuLevelOne['child'] as $keyLevelTwo => $menuLevelTwo) {
             if (isset($menuLevelTwo['child'])) {
 
-                $xhtml .= sprintf('<li><a href="%s">%s</a><ul>', $menuLevelTwo['link'], $menuLevelTwo['name']);
+                $xhtml .= sprintf('<li data-parent="%s"><a href="%s">%s</a><ul>', $keyLevelOne, $menuLevelTwo['link'], $menuLevelTwo['name']);
 
                 foreach ($menuLevelTwo['child'] as $keyLevelThree => $menuLevelThree) {
-                    $xhtml .= sprintf('<li><a href="%s">%s</a></li>', $menuLevelThree['link'], $menuLevelThree['name']);
+                    $xhtml .= sprintf('<li data-parent="%s"><a href="%s">%s</a></li>', $keyLevelOne, $menuLevelThree['link'], $menuLevelThree['name']);
                 }
                 $xhtml .= '</ul></li>';
             } else {
-                $xhtml .= sprintf('<li><a href="%s">%s</a></li>', $menuLevelTwo['link'], $menuLevelTwo['name']);
+                $xhtml .= sprintf('<li data-parent="%s"><a href="%s">%s</a></li>', $keyLevelOne, $menuLevelTwo['link'], $menuLevelTwo['name']);
             }
         }
         $xhtml .= '</ul></li>';
     } else {
         $classActive = ($menuCurrent == $keyLevelOne) ? 'class="active"' : '';
-        $xhtml .= sprintf('<li><a href="%s">%s</a></li>', $menuLevelOne['link'], $menuLevelOne['name']);
+        $xhtml .= sprintf('<li data-name="%s"><a href="%s">%s</a></li>', $keyLevelOne, $menuLevelOne['link'], $menuLevelOne['name']);
     }
 }
 ?>
